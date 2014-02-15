3ed604161f02e36a62f515d1cc9502c233aee784
Dan Peddle <dan.peddle@gmail.com>
2014-02-15 21:48:53 +0100

    cleanup

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-const A = 440;
+var App = require('./lib/app');
+var clog = console.log.bind(console);
 
-var App = require('./main');
-var app = new App();
+var audioContext = require('./lib/audiocontext');
 
-app.add([]);
+var A = 440; // set A to be 440hz
+
+
+
+var AudioApp = App.extend({
+	events: {
+		'play':'play',
+		'pause':'pause'
+	},
+	initialize: function(options){
+		this.context = audioContext;
+		this.chain = [];
+	},
+	play: function(){
+		clog('play');
+	},
+	pause: function(){
+		clog('pause');
+	},
+	add: function (comp){
+		this.bus.emit('pause');
+		this.chain.push(comp);
+		process.nextTick(this.play.bind(this));
+	}
+
+});
+
+
+module.exports = AudioApp;
---
+++ index.js
@@ -0,0 +1,6 @@
+const A = 440;
+
+var App = require('./app');
+var app = new App();
+
+app.add([]);
